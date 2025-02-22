20d4b3c0d71e1b79c64e97f74fc5f0bb34ef915c
travistrue2008 <travis.true08@gmail.com>
2025-02-22 15:29:21 -0800

    Added output routes.

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,6 @@
 import {
   middleware,
-  createServer,
+  createApp,
 } from 'sleepy-serv'
 
 const PORT = 3000
@@ -11,7 +11,7 @@ middleware.setValidationFormats({
   state: /^(([A][ELKSZR])|([C][AOT])|([D][EC])|([F][ML])|([G][AU])|([H][I])|([I][DLNA])|([K][SY])|([L][A])|([M][EHDAINSOT])|([N][EVHJMYCD])|([M][P])|([O][HKR])|([P][WAR])|([R][I])|([S][CD])|([T][NX])|([U][T])|([V][TIA])|([W][AVIY]))$/,
 })
 
-const routes = await createServer(PORT, import.meta.dirname, {
+const routes = await createApp(PORT, import.meta.dirname, {
   mountPath: '/api',
   middleware: [
     _req => console.info('root-level middleware'),
