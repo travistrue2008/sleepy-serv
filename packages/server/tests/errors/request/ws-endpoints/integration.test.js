5addde28a73b8b8d612c5675a212be34e6727574
travistrue2008 <travis.true08@gmail.com>
2026-07-16 15:38:41 -0700

    Added more ergonomic helpers to server integration tests

--- packages/server/tests/errors/request/ws-endpoints/integration.test.js
+++ packages/server/tests/errors/request/ws-endpoints/integration.test.js
@@ -1,16 +1,26 @@
 import { describe, test, expect } from 'bun:test'
-import { FMT, Context } from '../../../helpers'
-import { NotFoundError, UnprocessableContentError } from '../../../../src'
+import { FMT, createRequestor, createSocketClient } from '../../../helpers'
+
+import {
+  NotFoundError,
+  UnauthorizedError,
+  UnprocessableContentError,
+  createApp,
+} from '../../../../src'
 
 const CLIENT_ID_INVALID = 'client-invalid'
 const TICKET_INVALID = 'ticket-invalid'
+const TOKEN_INVALID = 'token-invalid'
 
 describe('POST', () => {
   test('when requested (REST)', async () => {
-    const ctx = await Context.create(import.meta.dirname)
-    const res = await ctx.makeRequest('/ws', FMT.JSON, { method: 'POST' })
+    const app = await createApp(0, import.meta.dirname)
+    const req = createRequestor(app)
+    const res = await req.post('/ws', FMT.JSON)
 
-    await ctx.shutdown()
+    console.log('url:', app.server.url)
+
+    await app.server.stop(true)
 
     expect(res.status).toBe(200)
 
@@ -22,14 +32,15 @@ describe('POST', () => {
   })
 
   test('when requested (ws)', async () => {
-    const ctx = await Context.create(import.meta.dirname)
-    const res = await ctx.sendMessage('POST', '/ws')
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+    const msg = await ws.post('/ws')
 
-    await ctx.shutdown()
+    await app.server.stop(true)
 
-    expect(res.status).toBe(UnprocessableContentError.status)
+    expect(msg.status).toBe(UnprocessableContentError.status)
 
-    expect(res.body).toStrictEqual([
+    expect(msg.body).toStrictEqual([
       {
         path: '',
         message: 'must NOT be valid',
@@ -39,49 +50,177 @@ describe('POST', () => {
 })
 
 describe('PUT', () => {
-  // test('when requested (REST)', async () => {
-  //   const ctx = await Context.create(import.meta.dirname)
+  test('when the "authorization" header is missing (REST)', async () => {
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+    const req = createRequestor(app)
 
-  //   const res = await ctx.makeRequest('/ws', FMT.JSON, {
-  //     method: 'PUT',
-  //     query: {
-  //     },
-  //   })
+    const res = await req.put(`/ws/${ws.clientId}`, FMT.JSON, {
+      headers: new Headers({}),
+    })
 
-  //   await ctx.shutdown()
+    await app.server.stop(true)
 
-  //   expect(res.status).toBe(200)
+    expect(res.status).toBe(UnprocessableContentError.status)
 
-  //   expect(res.body).toStrictEqual({
-  //     clientId: expect.any(String),
-  //     ticket: expect.any(String),
-  //     data: null,
-  //   })
-  // })
+    expect(res.body).toStrictEqual([
+      {
+        path: 'headers',
+        message: `must have required property 'authorization'`,
+      },
+    ])
+  })
 
-  // test('when requested (ws)', async () => {
-  //   const ctx = await Context.create(import.meta.dirname)
-  //   const res = await ctx.sendMessage('POST', '/ws')
+  test('when the "authorization" header is missing (ws)', async () => {
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
 
-  //   await ctx.shutdown()
+    const msg = await ws.put(`/ws/${ws.clientId}`, {
+      headers: new Headers({}),
+    })
 
-  //   expect(res.status).toBe(UnprocessableContentError.status)
+    await app.server.stop(true)
 
-  //   expect(res.body).toStrictEqual([
-  //     {
-  //       path: '',
-  //       message: 'must NOT be valid',
-  //     },
-  //   ])
-  // })
+    expect(msg.status).toBe(UnprocessableContentError.status)
+
+    expect(msg.body).toStrictEqual([
+      {
+        path: '',
+        message: 'must NOT be valid',
+      },
+    ])
+  })
+
+  test('when the "clientId" param is invalid (REST)', async () => {
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+    const req = createRequestor(app)
+
+    const res = await req.put(`/ws/${CLIENT_ID_INVALID}`, FMT.JSON, {
+      headers: new Headers({
+        authorization: `Bearer ${ws.token}`,
+      }),
+    })
+
+    await app.server.stop(true)
+
+    expect(res.status).toBe(NotFoundError.status)
+    expect(res.body).toStrictEqual(null)
+  })
+
+  test('when the "clientId" param is invalid (ws)', async () => {
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+
+    const msg = await ws.put(`/ws/${CLIENT_ID_INVALID}`, {
+      headers: new Headers({
+        authorization: `Bearer ${ws.token}`,
+      }),
+    })
+
+    await app.server.stop(true)
+
+    expect(msg.status).toBe(UnprocessableContentError.status)
+
+    expect(msg.body).toStrictEqual([
+      {
+        path: '',
+        message: 'must NOT be valid',
+      },
+    ])
+  })
+
+  test('when the "token" header is incorrect (REST)', async () => {
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+    const req = createRequestor(app)
+
+    const res = await req.put(`/ws/${ws.clientId}`, FMT.JSON, {
+      headers: new Headers({
+        authorization: `Bearer ${TOKEN_INVALID}`,
+      }),
+    })
+
+    await app.server.stop(true)
+
+    expect(res.status).toBe(UnauthorizedError.status)
+    expect(res.body).toStrictEqual(null)
+  })
+
+  test('when the "token" header is incorrect (ws)', async () => {
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+
+    const msg = await ws.put(`/ws/${ws.clientId}`, {
+      headers: new Headers({
+        authorization: `Bearer ${TOKEN_INVALID}`,
+      }),
+    })
+
+    await app.server.stop(true)
+
+    expect(msg.status).toBe(UnprocessableContentError.status)
+
+    expect(msg.body).toStrictEqual([
+      {
+        path: '',
+        message: 'must NOT be valid',
+      },
+    ])
+  })
+
+  test('when requested (REST)', async () => {
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+    const req = createRequestor(app)
+
+    const res = await req.put(`/ws/${ws.clientId}`, FMT.JSON, {
+      headers: new Headers({
+        authorization: `Bearer ${ws.token}`,
+      }),
+    })
+
+    await app.server.stop(true)
+
+    expect(res.status).toBe(200)
+
+    expect(res.body).toStrictEqual({
+      clientId: expect.any(String),
+      ticket: expect.any(String),
+      data: null,
+    })
+  })
+
+  test('when requested (ws)', async () => {
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+
+    const msg = await ws.put(`/ws/${ws.clientId}`, {
+      headers: new Headers({
+        authorization: `Bearer ${ws.token}`,
+      }),
+    })
+
+    await app.server.stop(true)
+
+    expect(msg.status).toBe(UnprocessableContentError.status)
+
+    expect(msg.body).toStrictEqual([
+      {
+        path: '',
+        message: 'must NOT be valid',
+      },
+    ])
+  })
 })
 
 describe('GET', () => {
   test('when NO "ticket" querystring (REST)', async () => {
-    const ctx = await Context.create(import.meta.dirname)
-    const res = await ctx.makeRequest('/ws', FMT.JSON, { method: 'GET' })
+    const app = await createApp(0, import.meta.dirname)
+    const req = createRequestor(app)
+    const res = await req.get('/ws', FMT.JSON)
 
-    await ctx.shutdown()
+    await app.server.stop(true)
 
     expect(res.status).toBe(UnprocessableContentError.status)
 
@@ -94,14 +233,15 @@ describe('GET', () => {
   })
 
   test('when NO "ticket" querystring (ws)', async () => {
-    const ctx = await Context.create(import.meta.dirname)
-    const res = await ctx.sendMessage('GET', '/ws')
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+    const msg = await ws.get('/ws')
 
-    await ctx.shutdown()
+    await app.server.stop(true)
 
-    expect(res.status).toBe(UnprocessableContentError.status)
+    expect(msg.status).toBe(UnprocessableContentError.status)
 
-    expect(res.body).toStrictEqual([
+    expect(msg.body).toStrictEqual([
       {
         path: '',
         message: 'must NOT be valid',
@@ -110,16 +250,16 @@ describe('GET', () => {
   })
 
   test('when invalid "ticket" querystring (REST)', async () => {
-    const ctx = await Context.create(import.meta.dirname)
+    const app = await createApp(0, import.meta.dirname)
+    const req = createRequestor(app)
 
-    const res = await ctx.makeRequest('/ws', FMT.JSON, {
-      method: 'GET',
+    const res = await req.get('/ws', FMT.JSON, {
       query: {
         ticket: TICKET_INVALID,
       },
     })
 
-    await ctx.shutdown()
+    await app.server.stop(true)
 
     expect(res.status).toBe(NotFoundError.status)
 
@@ -128,14 +268,15 @@ describe('GET', () => {
 
   test('when invalid "ticket" querystring (ws)', async () => {
     const url = `/ws?ticket=${TICKET_INVALID}`
-    const ctx = await Context.create(import.meta.dirname)
-    const res = await ctx.sendMessage('GET', url)
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+    const msg = await ws.get(url)
 
-    await ctx.shutdown()
+    await app.server.stop(true)
 
-    expect(res.status).toBe(UnprocessableContentError.status)
+    expect(msg.status).toBe(UnprocessableContentError.status)
 
-    expect(res.body).toStrictEqual([
+    expect(msg.body).toStrictEqual([
       {
         path: '',
         message: 'must NOT be valid',
@@ -144,18 +285,16 @@ describe('GET', () => {
   })
 
   test('when providing a "ticket" querystring (REST)', async () => {
-    const ctx = await Context.create(import.meta.dirname)
+    const app = await createApp(0, import.meta.dirname)
+    const req = createRequestor(app)
 
-    const { ticket } = await ctx.makeRequest('/ws', FMT.JSON, {
-      method: 'POST',
-    })
+    const { ticket } = await req.post('/ws', FMT.JSON)
 
-    const res = await ctx.makeRequest('/ws', FMT.JSON, {
-      method: 'GET',
+    const res = await req.get('/ws', FMT.JSON, {
       query: { ticket },
     })
 
-    await ctx.shutdown()
+    await app.server.stop(true)
 
     expect(res.status).toBe(NotFoundError.status)
 
@@ -163,21 +302,16 @@ describe('GET', () => {
   })
 
   test('when providing a "ticket" querystring (ws)', async () => {
-    const ctx = await Context.create(import.meta.dirname)
+    const app = await createApp(0, import.meta.dirname)
+    const ws = await createSocketClient(app)
+    const { ticket } = await ws.post('/ws', FMT.JSON, { method: 'POST' })
+    const msg = await ws.get('/ws', { query: { ticket } })
 
-    const { ticket } = await ctx.makeRequest('/ws', FMT.JSON, {
-      method: 'POST',
-    })
+    await app.server.stop(true)
 
-    const res = await ctx.sendMessage('GET', '/ws', {
-      query: { ticket },
-    })
+    expect(msg.status).toBe(UnprocessableContentError.status)
 
-    await ctx.shutdown()
-
-    expect(res.status).toBe(UnprocessableContentError.status)
-
-    expect(res.body).toStrictEqual([
+    expect(msg.body).toStrictEqual([
       {
         path: '',
         message: 'must NOT be valid',
