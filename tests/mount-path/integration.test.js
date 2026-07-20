053952506c20d0b5cd53f93e109e4311c05ae9cf
Travis J True <travis.true08@gmail.com>
2026-07-19 20:43:56 -0700

    Did some more E2E test refactoring. Added REST test counterpart for E2E scenarios that had none.

--- tests/mount-path/integration.test.js
+++ tests/mount-path/integration.test.js
@@ -1,57 +1,61 @@
-import { test, expect } from 'bun:test'
+import { describe, test, expect } from 'bun:test'
 import { createApp } from 'sleepy-serv'
 import { FMT, createRequestor } from '../helpers'
 import SleepySocketClient, { TYPES } from 'sleepy-socket'
 
 const MOUNT_PATH = '/test-mount-path'
 
-test('when "mouthPath" is set (REST)', async () => {
-  const app = await createApp(0, import.meta.dirname, {
-    mountPath: MOUNT_PATH,
+describe('REST', () => {
+  test('when "mouthPath" is set', async () => {
+    const app = await createApp(0, import.meta.dirname, {
+      mountPath: MOUNT_PATH,
+    })
+
+    const req = createRequestor(app)
+
+    const res = await req.get('/', FMT.JSON, {
+      mountPath: MOUNT_PATH,
+    })
+
+    await app.server.stop(true)
+
+    expect(res.status).toBe(200)
+    expect(res.body).toStrictEqual({ ok: true })
   })
-
-  const req = createRequestor(app)
-
-  const res = await req.get('/', FMT.JSON, {
-    mountPath: MOUNT_PATH,
-  })
-
-  await app.server.stop(true)
-
-  expect(res.status).toBe(200)
-  expect(res.body).toStrictEqual({ ok: true })
 })
 
-test('when "mouthPath" is set (ws)', async () => {
-  const app = await createApp(0, import.meta.dirname, {
-    mountPath: MOUNT_PATH,
-  })
+describe('WebSocket', () => {
+  test('when "mouthPath" is set', async () => {
+    const app = await createApp(0, import.meta.dirname, {
+      mountPath: MOUNT_PATH,
+    })
 
-  const host = app.server.url.hostname
+    const host = app.server.url.hostname
 
-  const client = await SleepySocketClient.connect(host, app.server.port, {
-    mountPath: MOUNT_PATH,
-  })
+    const client = await SleepySocketClient.connect(host, app.server.port, {
+      mountPath: MOUNT_PATH,
+    })
 
-  const res = await client.send({
-    method: 'GET',
-    route: '/',
-  })
+    const res = await client.send({
+      method: 'GET',
+      route: '/',
+    })
 
-  await client.close()
-  await app.server.stop(true)
+    await client.close()
+    await app.server.stop(true)
 
-  expect(res.status).toBe(200)
+    expect(res.status).toBe(200)
 
-  expect(res).toStrictEqual({
-    id: res.id,
-    clientId: client.id,
-    type: TYPES.RESPONSE,
-    status: 200,
-    timestamp: res.timestamp,
-    headers: {
-      'content-type': 'application/json;charset=utf-8',
-    },
-    body: { ok: true },
+    expect(res).toStrictEqual({
+      id: res.id,
+      clientId: client.id,
+      type: TYPES.RESPONSE,
+      status: 200,
+      timestamp: res.timestamp,
+      headers: {
+        'content-type': 'application/json;charset=utf-8',
+      },
+      body: { ok: true },
+    })
   })
 })
