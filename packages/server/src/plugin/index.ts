75b9f8c1d84fa28ba08c4b11f9a0ad157404e979
Travis True <travis.true08@gmail.com>
2026-09-09 20:40:09 -0700

    Added unit tests for plugin

--- packages/server/src/plugin/index.ts
+++ packages/server/src/plugin/index.ts
@@ -5,30 +5,23 @@ import { scanRoutes } from './scanner'
 import { generateBarrelModule } from './codegen'
 
 const config = await loadConfig()
-
-const apiRoot = path.resolve(
-  process.cwd(),
-  config.app?.root ?? './src/api',
-)
-
+const relativeRoot = config.app?.root ?? './src/api'
+const apiRoot = path.resolve(process.cwd(), relativeRoot)
 const barrelPath = require.resolve('sleepy-serv')
+const escaped = barrelPath.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
 
-const escaped = barrelPath
-  .replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+const constraints = {
+  filter: new RegExp(escaped),
+}
 
 plugin({
   name: 'sleepy-serv',
   setup (build) {
-    build.onLoad(
-      { filter: new RegExp(escaped) },
-      () => {
-        const scanResult = scanRoutes(apiRoot)
+    build.onLoad(constraints, () => {
+      const scanResult = scanRoutes(apiRoot)
+      const contents = generateBarrelModule(scanResult)
 
-        return {
-          contents: generateBarrelModule(scanResult),
-          loader: 'ts',
-        }
-      },
-    )
+      return { loader: 'ts', contents }
+    })
   },
 })
